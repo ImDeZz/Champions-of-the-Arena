613fda6f8f2c890ea524fb71f05fb1ad43ca5004
dezso15 <dezso15@gmail.com>
2018-09-29 18:55:28 +0200

    Add Unity Project

---
+++ Champions of the Arena/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Movement : MonoBehaviour
+{
+    const float speed = 20.0f;
+
+    VirtualJoystick joystick; 
+    CharacterController cC;
+
+    // Use this for initialization
+    void Start()
+    {
+        joystick = GameObject.Find("VirtualJoystick").GetComponent<VirtualJoystick>();
+        cC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        cC.Move(joystick.InputDirection*speed*Time.deltaTime);
+	}
+
+}
