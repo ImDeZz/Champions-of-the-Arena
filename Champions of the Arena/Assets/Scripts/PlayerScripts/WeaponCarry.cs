ae9211f6e8317ff2abaf79b2d8c72b848e442459
dezso15 <dezso15@gmail.com>
2018-10-06 20:00:27 +0200

    Add Dynamic Weapon Positioner

---
+++ Champions of the Arena/Assets/Scripts/PlayerScripts/WeaponCarry.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCarry : MonoBehaviour {
+
+    GameObject[] weapons;
+    GameObject player;
+    bool[] weaponBeingCarried;
+    bool carryWeapon = false;
+    Dictionary<string, int> playerPerWeapon = new Dictionary<string, int>(); //PlayerName, WeaponIndex
+
+	void Start ()
+    {
+        player = GameObject.Find("Player");
+        weapons = GameObject.FindGameObjectsWithTag("Weapons");
+        weaponBeingCarried = new bool[weapons.Length];
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            weaponBeingCarried[i] = false;
+        }
+	}
+	
+	void Update ()
+    {
+        if (carryWeapon)
+        {
+            weapons[playerPerWeapon[player.name]].transform.localPosition = player.transform.localPosition;
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!carryWeapon)
+        {
+            Debug.Log(collision.gameObject.name);
+            if (collision.gameObject.tag == "Weapons")
+            {
+                int weaponIndex = int.Parse(collision.gameObject.name);
+                playerPerWeapon[player.name] = weaponIndex;
+                weaponBeingCarried[weaponIndex] = true;
+                carryWeapon = true;
+            }
+        }
+    }
+
+    public void DropWeapon()
+    {
+
+    }
+
+}
